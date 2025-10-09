b9ce911e97cc23950b5db8fa1624ac9025e87a53
Blockhead2000 <ctrombetta25@elam.co.uk>
2025-10-09 12:46:46 +0100

    push

--- Assets/MainMenu1.cs
+++ Assets/MainMenu1.cs
@@ -1,22 +1,23 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    [Header("Scene Name")]
-    public string gameSceneName = "GameScene";
+   [Header("Scene Names")]
+    public string gameSceneName = "MainGame";
+    
 
     public void PlayGame()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(gameSceneName);
-
     }
 
     public void QuitGame()
     {
         Debug.Log("Quit Game");
         Application.Quit();
+        Debug.Log("QuitPressed");
     }
 }
-
